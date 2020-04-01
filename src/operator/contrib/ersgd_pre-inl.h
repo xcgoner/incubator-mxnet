a8d3111494a26c3c270fbd17ecb757193ecd2187
unknown <xcgoner1108@gmail.com>
2020-04-01 15:10:48 -0500

    ersgd_pre

--- src/operator/contrib/ersgd_pre-inl.h
+++ src/operator/contrib/ersgd_pre-inl.h
@@ -40,6 +40,7 @@ struct ERSGDPreParam : public dmlc::Parameter<ERSGDPreParam> {
   float rescale_grad;
   float clip_gradient;
   bool  nesterov;
+  bool  layer_sparse;
   DMLC_DECLARE_PARAMETER(ERSGDPreParam) {
     DMLC_DECLARE_FIELD(lr)
     .describe("Learning rate");
